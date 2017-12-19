021d5ee0113613eb17807221daea13fab7cb87b9
Pawel Zun <pzun90@gmail.com>
2017-12-19 23:17:37 +0100

    debugging

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -12,27 +12,5 @@ var ContactForm = React.createClass({
         <button type={'submit'}>Dodaj kontakt</button>
       </form>
     )
-  },
-})
-
-/*
-      
-      React.createElement('form', {className: 'contactForm'},
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'Imię',
-          value: this.props.contact.firstName,
-        }),
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'Nazwisko',
-          value: this.props.contact.lastName,
-        }),
-        React.createElement('input', {
-          type: 'email',
-          placeholder: 'Email',
-          value: this.props.contact.email,
-        }),
-        React.createElement('button', {type: 'submit'}, "Dodaj kontakt")
-      )
-*/
+  }
+});
