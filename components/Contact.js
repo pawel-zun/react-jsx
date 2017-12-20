7ae9fdfabb1cd4b1a6f906233ab09c22bdee8387
Pawel Zun <pzun90@gmail.com>
2017-12-20 17:03:42 +0100

    make another JSX

--- components/Contact.js
+++ components/Contact.js
@@ -19,21 +19,6 @@ var Contact = React.createClass({
           </a>
         </div>
       </li>
-    )
+    );
   },
 });
-
-/*
-React.createElement('li', {className: 'contactItem'},
-        React.createElement('i', {
-          className: 'fa fa-address-card'
-        }),
-        React.createElement('div', {className: 'contact-container'},
-          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-            this.props.item.email
-          )
-        )        
-      )
-*/
--- components/Contacts.js
+++ components/Contacts.js
@@ -5,11 +5,13 @@ var Contacts = React.createClass({
 
   render: function() {
     var contacts = this.props.items.map(function(contact) {
-        return React.createElement(Contact, {item: contact, key: contact.id});
+      <Contact item={contact} key: contact.id} />;
     });
 
     return (
-      React.createElement('ul', {className: 'contactsList'}, contacts)
+      <ul className={'contactsList'}
+      contacts
+      </ul>
     );
   }
 });
